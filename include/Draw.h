86b081ad968a20e739e7c40ff14d324bd2b61334
Wouter Wijsman <wwijsman@live.nl>
2022-04-19 15:09:32 +0200

    Removed unused Blit function

--- include/Draw.h
+++ include/Draw.h
@@ -8,7 +8,6 @@ class Draw {
 public:
 
     static void BlitSquare(SDL_Renderer * renderer, SDL_Texture * texture, int x, int y, int dx, int dy);
-    static void Blit(SDL_Renderer * renderer, SDL_Texture * texture, int x, int y, int dx, int dy);
     static void BlitOther(SDL_Renderer * renderer, SDL_Texture * texture, int x, int y, int dx, int dy, int w, int h);
     static void BlackSquare(SDL_Renderer * renderer, int x, int y);
     static void Flip(SDL_Renderer * renderer);
--- src/Draw.cpp
+++ src/Draw.cpp
@@ -14,10 +14,6 @@ void Draw::BlitSquare(SDL_Renderer * renderer, SDL_Texture * texture, int x, int
     SDL_RenderCopy(renderer, texture, &sr, &dr);
 }
 
-void Draw::Blit(SDL_Renderer * renderer, SDL_Texture * texture, int x, int y, int dx, int dy) {
-
-}
-
 void Draw::BlitOther(SDL_Renderer * renderer, SDL_Texture * texture, int x, int y, int dx, int dy, int w, int h) {
     SDL_Rect sr={x,y,w,h};
     SDL_Rect dr={dx,dy,w, h};
