9e139f2ed9162985410c7b1b5c4067dd27e3b029
Wouter Wijsman <wwijsman@live.nl>
2022-04-26 16:59:23 +0200

    Remove unused constant

--- include/constants.h
+++ include/constants.h
@@ -18,8 +18,6 @@ inline constexpr int IMAGE_WIDTH = 40;
 
 inline constexpr int BEAM_PERSISTANCE = 4;		//beam lasts for this many 0.025 second intervals
 
-inline constexpr int COLOR_DEPTH = 16;
-
 inline constexpr char* NAME = (char*) "TANK";
 inline constexpr char* TITLE = (char*) "Laser Kombat";
 
